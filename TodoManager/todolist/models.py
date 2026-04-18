e2fc86fa8f604e0e074655d136d359e083c9d432
pravin5062804 <pravin.choudhary.5062804@ves.ac.in>
2026-04-19 01:34:42 +0530

    learn about migrations and models creation and mysql

--- TodoManager/todolist/models.py
+++ TodoManager/todolist/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class task(models.Model):
+    task=models.CharField(max_length=100)
+    is_completed=models.BooleanField()
+    
+    def __str__(self):
+     return self.task
+    
